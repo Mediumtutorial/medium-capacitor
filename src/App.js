b567b6f77bccde0ad9c60688a43b7c19d7325b6c
manntrix <MANISH.MANDAL21@GMAIL.COM>
2020-04-24 14:56:36 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Medium Tutorial</h1>
+     <p>Convert your existing react js app to android or ios app using the capacitor</p>
+    </div>
+  );
+}
+
+export default App;
